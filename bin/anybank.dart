be18848116961ef2bf098eeb8986ad191bf49341
Sérvulo Pires da Rocha Júnior <servulo.jr@gmail.com>
2025-11-28 16:55:16 -0300

    Finalizando o treinamento

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,18 +1,19 @@
 import 'package:anybank/conta.dart';
 
 void main() {
+  ContaCorrente contaChris = ContaCorrente("Chris", 4000);
+  ContaPoupanca contaDenize = ContaPoupanca("Denize", 4000);
+  ContaEmpresa contaMatheus = ContaEmpresa("Matheus", 2000);
+  ContaInvestimento contaRoberta = ContaInvestimento("Roberta", 2000);
 
-  Conta contaMatheus = Conta("Matheus", 1000);
-  Conta contaRoberta = Conta("Roberta", 2000);
+  contaChris.imprimeSaldo();
+  contaChris.enviar(4300);
+  
+  contaDenize.imprimeSaldo();
+  contaDenize.enviar(4300);
+  contaDenize.calculaRendimento();
+  contaDenize.imprimeSaldo();
 
-  List<Conta> contas = [contaMatheus, contaRoberta];
-
-  for(Conta conta in contas) {
-    conta.imprimeSaldo();
-  }
-
-  contaRoberta.receber(3000);
-  contaMatheus.receber(500);
-  contaMatheus.enviar(200);
-
+  contaMatheus.enviar(1000);
+  contaRoberta.receber(1000);
 }
--- lib/conta.dart
+++ lib/conta.dart
@@ -1,5 +1,4 @@
-class Conta {
-
+abstract class Conta {
   String titular;
   double _saldo;
 
@@ -14,11 +13,80 @@ class Conta {
     if (valor <= _saldo) {
       _saldo -= valor;
       imprimeSaldo();
-    } 
+    }
   }
 
   void imprimeSaldo() {
     print("O saldo atual de $titular é: R\$$_saldo");
+  }
+}
+
+class ContaCorrente extends Conta {
+  double emprestimo = 300;
+
+  ContaCorrente(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if (_saldo + emprestimo >= valor) {
+      _saldo -= valor;
+      imprimeSaldo();
+    }
+  }
+}
+
+class ContaPoupanca extends Conta {
+  double rendimento = 0.05;
+
+  ContaPoupanca(super.titular, super._saldo);
+
+  void calculaRendimento() {
+    _saldo += _saldo * rendimento;
+  }
+}
+
+mixin Imposto {
+  double taxa = 0.03;
+
+  double valorTaxado(double valor) {
+    return valor * taxa;
+  }
+}
+
+class ContaEmpresa extends Conta with Imposto {
+  ContaEmpresa(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if (_saldo >= valor + valorTaxado(valor)) {
+      _saldo -= (valor + valorTaxado(valor));
+      imprimeSaldo();
+    }
+  }
+
+  @override
+  void receber(double valor) {
+    _saldo += valor - valorTaxado(valor);
+    imprimeSaldo();
+  }  
+}
+
+class ContaInvestimento extends Conta with Imposto {
+  
+  ContaInvestimento(super.titular, super._saldo);
+
+  @override
+  void enviar(double valor) {
+    if (_saldo >= valor + valorTaxado(valor)) {
+      _saldo -= (valor + valorTaxado(valor));
+      imprimeSaldo();
+    }
+  }
+
+  @override
+  void receber(double valor) {
+    _saldo += valor - valorTaxado(valor);
+    imprimeSaldo();
   }
 
 }
