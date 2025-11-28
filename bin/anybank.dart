e7e64f854d667ef98cec3c3fa33b2f297771526c
Sérvulo Pires da Rocha Júnior <servulo.jr@gmail.com>
2025-11-28 00:17:54 -0300

    first commit

---
+++ bin/anybank.dart
@@ -0,0 +1,18 @@
+import 'package:anybank/conta.dart';
+
+void main() {
+
+  Conta contaMatheus = Conta("Matheus", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
+
+  List<Conta> contas = [contaMatheus, contaRoberta];
+
+  for(Conta conta in contas) {
+    conta.imprimeSaldo();
+  }
+
+  contaRoberta.receber(3000);
+  contaMatheus.receber(500);
+  contaMatheus.enviar(200);
+
+}
---
+++ lib/conta.dart
@@ -0,0 +1,24 @@
+class Conta {
+
+  String titular;
+  double _saldo;
+
+  Conta(this.titular, this._saldo);
+
+  void receber(double valor) {
+    _saldo += valor;
+    imprimeSaldo();
+  }
+
+  void enviar(double valor) {
+    if (valor <= _saldo) {
+      _saldo -= valor;
+      imprimeSaldo();
+    } 
+  }
+
+  void imprimeSaldo() {
+    print("O saldo atual de $titular é: R\$$_saldo");
+  }
+
+}
